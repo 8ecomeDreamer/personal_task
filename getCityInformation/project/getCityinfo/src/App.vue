6e20c20dc48deebbc9b37a08b8fc3f7f275435ee
kkkkk0630 <kyriejason@outlook.com>
2022-10-17 23:52:13 +0800

    updated

--- getCityInformation/project/getCityinfo/src/App.vue
+++ getCityInformation/project/getCityinfo/src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <div class="wrapper">
+      <UrbanInfo/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import UrbanInfo from './components/UrbanInfo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UrbanInfo
   }
 }
 </script>
 
-<style>
-#app {
+<style scoped>
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+  .wrapper{
+    width:1873px;
+    height:937px;
+    /* background:black; */
+  }
 </style>
