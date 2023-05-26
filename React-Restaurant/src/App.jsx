90257958ea794f135d2fc11418c2529f782c315d
8ecomeDreamer <1412415464@qq.com>
2023-05-26 15:20:19 +0800

    实现初级路由

--- React-Restaurant/src/App.jsx
+++ React-Restaurant/src/App.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import Contact from "./components/homePage/Contact";
-import Header from "./components/homePage/Header";
-import Home from "./components/homePage/Home";
-import About from "./components/homePage/About";
-import NavIcon from "./components/homePage/NavIcon";
-import Recipe from "./components/homePage/Recipe";
-import Footer from "./components/homePage/Footer";
-import Content from "./components/homePage/Content";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "@/views/homePage";
 
 const App = () => {
   return (
-    <div className="font-poppins bg-back">
-      <Header />
-      <Contact />
-      <Content />
-      {/* <NavIcon /> */}
-      {/* <Home /> */}
-      {/* <About /> */}
-      {/* <Recipe /> */}
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ React-Restaurant/src/views/homePage/homePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Contact from "@/components/homePage/Contact";
+import Header from "@/components/homePage/Header";
+// import Home from "../components/homePage/Home";
+// import About from "../components/homePage/About";
+// import NavIcon from "../components/homePage/NavIcon";
+// import Recipe from "../components/homePage/Recipe";
+import Footer from "@/components/homePage/Footer";
+import Content from "@/components/homePage/Content";
+
+const HomePage = () => {
+  return (
+    <div className="font-poppins bg-back">
+      <Header />
+      <Contact />
+      <Content />
+      {/* <NavIcon /> */}
+      {/* <Home /> */}
+      {/* <About /> */}
+      {/* <Recipe /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
