ebf4da3dad8561c12da2afade1798b9d5082481f
8ecomeDreamer <1412415464@qq.com>
2023-05-26 15:09:39 +0800

    配置alias

--- React-Restaurant/src/App.jsx
+++ React-Restaurant/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Contact from "./components/Contact";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import About from "./components/About";
-import NavIcon from "./components/NavIcon";
-import Recipe from "./components/Recipe";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
+import Contact from "./components/homePage/Contact";
+import Header from "./components/homePage/Header";
+import Home from "./components/homePage/Home";
+import About from "./components/homePage/About";
+import NavIcon from "./components/homePage/NavIcon";
+import Recipe from "./components/homePage/Recipe";
+import Footer from "./components/homePage/Footer";
+import Content from "./components/homePage/Content";
 
 const App = () => {
   return (
